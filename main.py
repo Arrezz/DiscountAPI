182f9fd6c33bb1897539c49f09ebd64c0a3670aa
Arrezz <hansarvid96@hotmail.se>
2021-10-26 22:48:51 +0200

    Redesigned with better data structure and some more errorhandling

--- main.py
+++ main.py
@@ -3,34 +3,36 @@ import uuid
 import flask
 from flask import Flask, request
 
-discount_codes = [
-    {
-        'product_id': 'd81e8989-647d-45fa-8aa9-b8dd68830ad7',
-        'discount_code': '983fcf98-0bd5-4fd3-9e71-5c9c334c8b1b'
-    }
-]
+discount_codes = [{'5739c588-2943-4748-a8d4-a7346f94a420': {'85219126-6d32-4ccc-b03a-c4de1c0d77e4'}}]
 
 api = Flask(__name__)
 
 
-@api.route('/discount', methods=['POST'])
+@api.route('/discount/post', methods=['GET'])
 def post_discount():
     if request.args.get('product_id') is None:
         flask.abort(400)
     product_id = request.args.get('product_id')
     discount_code = str(uuid.uuid4())
-    discount_codes.append(dict([('product_id', str(product_id)),
-                                ('discount_code', discount_code)]))
+    for index in range(len(discount_codes)):
+        try:
+            if product_id in discount_codes[index].keys():
+                discount_codes[index][product_id].add(discount_code)
+                return discount_code
+        except:
+            pass
+    discount_codes.append({product_id: {discount_code}})
     return discount_code
 
 
-@api.route('/discount', methods=['GET'])
+@api.route('/discount/get', methods=['GET'])
 def get_discount():
     if request.args.get('product_id') is None:
         return flask.abort(400)
+    product_id = request.args.get('product_id')
     for index in range(len(discount_codes)):
-        if discount_codes[index]['product_id'] == request.args.get('product_id'):
-            return str(discount_codes[index]['discount_code'])
+        if product_id in discount_codes[index].keys() and len(discount_codes[index][product_id]) != 0:
+            return discount_codes[index][product_id].pop()
     return flask.abort(404)
 
 
