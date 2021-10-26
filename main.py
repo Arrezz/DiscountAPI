997b7a2dedce84b74e1f93098e45e0ea549d99a6
Arrezz <hansarvid96@hotmail.se>
2021-10-26 20:07:18 +0200

    Initial implementation

--- main.py
+++ main.py
@@ -1,23 +1,37 @@
-from flask import Flask, json, request, jsonify
 import uuid
 
-discount_codes = [{"product_id":"4d18db17-f1ce-4704-9b13-5cbe44a8c78d" , "discount_code": "c7aeeb72-8d9e-4a6a-834c-ed37003aa027"},
-                  {"product_id":"2536c821-35c8-427c-a7c7-d14a269d58e1" , "discount_code": "c7aeeb72-8d9e-4a6a-834c-ed37003aa027"}]
+import flask
+from flask import Flask, request
+
+discount_codes = [
+    {
+        'product_id': 'd81e8989-647d-45fa-8aa9-b8dd68830ad7',
+        'discount_code': '983fcf98-0bd5-4fd3-9e71-5c9c334c8b1b'
+    }
+]
 
 api = Flask(__name__)
 
 
-@api.route('/discount/post', methods=['GET'])
+@api.route('/discount/post', methods=['POST'])
 def post_discount():
-    data = {'product_id': str(uuid.uuid4()), 'discount_code': str(uuid.uuid4())}
-    discount_codes.append(json.dumps(data))
-    return json.dumps(discount_codes)
+    if request.args.get('product_id') is None:
+        flask.abort(400)
+    product_id = request.args.get('product_id')
+    discount_code = str(uuid.uuid4())
+    discount_codes.append(dict([('product_id', str(product_id)),
+                                ('discount_code', discount_code)]))
+    return discount_code
 
 
 @api.route('/discount/get', methods=['GET'])
 def get_discount():
-    product_id = request.args.get('product_id')
-    return json.dumps(discount_codes)
+    if request.args.get('product_id') is None:
+        return flask.abort(400)
+    for index in range(len(discount_codes)):
+        if discount_codes[index]['product_id'] == request.args.get('product_id'):
+            return str(discount_codes[index]['discount_code'])
+    return flask.abort(404)
 
 
 if __name__ == '__main__':
